e11679e2ff2cf8c3ac4fb574be39357f51468b14
Isaac Dias <isaacardias@gmail.com>
2020-06-16 22:17:56 -0300

    utilizando o eslint, prettier e editorCofig

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-  constructor () {
+  constructor() {
     this.server = express();
     this.middlewares();
-    this.routes();   
+    this.routes();
   }
 
   middlewares() {
