46206be440dd5bfdb4a602cbfacded636c4143ae
Isaac Dias <isaacardias@gmail.com>
2020-06-19 10:56:50 -0300

    criação do loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-import { Router} from 'express';
+import { Router } from 'express';
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World'});
-})
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Isaac Dias',
+    email: 'isaacdias@gmail.com',
+    password_hash: '1238712387',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
